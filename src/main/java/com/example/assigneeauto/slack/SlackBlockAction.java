1de1ef6b2e16fe76968f7f7e1eddb5f4ee3df784
Sw24sX <aleksandr.korolyov.99@mail.ru>
2023-02-11 15:53:06 +0500

    Удалил наработки по Slack

--- src/main/java/com/example/assigneeauto/slack/SlackBlockAction.java
+++
@@ -1,8 +0,0 @@
-package com.example.assigneeauto.slack;
-
-import com.slack.api.bolt.handler.builtin.BlockActionHandler;
-import com.slack.api.bolt.handler.builtin.SlashCommandHandler;
-
-public interface SlackBlockAction extends BlockActionHandler {
-    String getActionName();
-}
--- src/main/java/com/example/assigneeauto/slack/SlackCommand.java
+++
@@ -1,7 +0,0 @@
-package com.example.assigneeauto.slack;
-
-import com.slack.api.bolt.handler.builtin.SlashCommandHandler;
-
-public interface SlackCommand extends SlashCommandHandler {
-    String getCommandName();
-}
