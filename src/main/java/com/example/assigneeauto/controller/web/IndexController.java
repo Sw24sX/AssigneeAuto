ab57c3ed8e729adce437c6ddfc55b8c29b2427a8
Sw24sX <aleksandr.korolyov.99@mail.ru>
2023-02-18 15:10:38 +0500

    index страница

---
+++ src/main/java/com/example/assigneeauto/controller/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.assigneeauto.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String index(@RequestParam(name="name", required=false, defaultValue="Test") String name, Model model) {
+        model.addAttribute("name", name);
+        return "redirect:/reviewer/list";
+    }
+}
